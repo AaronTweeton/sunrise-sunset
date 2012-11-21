d5481b7f126e7050fdfce4fe2a7f6e765426cacc
Aaron Tweeton <aaron@tweeton.com>
2012-11-20 19:53:50 -0800

    Fixed error where theme was supposed to be showing single front page but was showing excerpt sticky post instead.

--- front-page.php
+++ front-page.php
@@ -9,18 +9,12 @@
 get_header() ?> 
 	<div class="row">
 		<div class="span12">
-			<?php query_posts(array (
-				'posts_per_page' => 1,
-				'post__in'  => get_option( 'sticky_posts' ),
-				'ignore_sticky_posts' => 1		
-			)); ?>  
 			<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<!-- Main hero unit for a primary marketing message or call to action -->
 				<div class="hero-unit">
 					<h1><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-					<?php the_excerpt(); ?>
-					<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+					<?php the_content(); ?>
 				</div>
 			<?php endwhile; ?>        
 			<?php else : ?>

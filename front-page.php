60d202b73f71000b334ca606df7a93b855b1ad7c
Aaron Tweeton <aaron@aarontweeton.com>
2012-11-21 06:28:46 -0800

    Removed the_title from front_page template.

--- front-page.php
+++ front-page.php
@@ -11,17 +11,13 @@ get_header() ?>
 		<div class="span12">
 			<?php if ( have_posts() ) : ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<!-- Main hero unit for a primary marketing message or call to action -->
 				<div class="hero-unit">
-					<h1><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 					<?php the_content(); ?>
 				</div>
 			<?php endwhile; ?>        
 			<?php else : ?>
 			<?php endif; // end have_posts() check ?>
 		</div>	
-	</div>
-        
+	</div>        
 	<?php get_sidebar(); ?> 
-
 <?php get_footer() ?>
